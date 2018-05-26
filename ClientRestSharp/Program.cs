4928a6a83684f3a6b42c9511d1cefd8c78924b23
antonvykhlo <antonvykhlo@gmail.com>
2018-05-26 20:59:19 +0300

    Rest client added

---
+++ ClientRestSharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientRestSharp
+{
+    class Program
+    {
+        public static void testPostAsync(int value)
+        {
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            string postData = "textField=" + value;
+            byte[] data = encoding.GetBytes(postData);
+
+            HttpWebRequest myRequest =
+              (HttpWebRequest)WebRequest.Create("http://localhost:49214/SendAsync/Index/");
+            myRequest.Method = "POST";
+            myRequest.ContentType = "application/x-www-form-urlencoded";
+            myRequest.ContentLength = data.Length;
+            Stream newStream = myRequest.GetRequestStream();
+            newStream.Write(data, 0, data.Length);
+            newStream.Close();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ready");
+            Console.ReadLine();
+            while (Console.ReadLine() != "q")
+            {
+                testPostAsync(132);
+                Console.WriteLine("Done");
+                Console.ReadLine();
+            }
+        }
+    }
+}
